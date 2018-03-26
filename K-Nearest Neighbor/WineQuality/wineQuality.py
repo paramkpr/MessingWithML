ebd4f0744471a041b83fa5ea8f8d9c0a48a470d6
Param Kapur <paramkapur2002@gmail.com>
2018-03-26 10:50:53 +0530

    modified:   K-Nearest Neighbor/BreastCancer/breast-cancer-wisconsin.names.txt Semicolons removed 	new file:   K-Nearest Neighbor/WineQuality/wineQuality.py  Predicts quality of wines on a scale of 1-12 	new file:   K-Nearest Neighbor/WineQuality/winequality-red.csv Data for wine classifer 	new file:   K-Nearest Neighbor/WineQuality/winequality-white.csv Data for wine classifier 	new file:   K-Nearest Neighbor/WineQuality/winequality.names.txt Data description for wine classifer

---
+++ K-Nearest Neighbor/WineQuality/wineQuality.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn import neighbors, preprocessing, model_selection
+import pandas as pd
+
+df = pd.read_csv('winequality-red.csv')
+df.astype(float).values.tolist()
+
+X = np.array(df.drop(['quality'], 1))
+y = np.array(df['quality'])
+
+X = preprocessing.scale(X)
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, y_train)
+
+print(clf.score(X, y))
+
+to_predict = np.array([7, 0.57, 0.23, 6.8, 0.09, 20, 104, 0.9978, 9.36, 0.9, 10.5]).reshape(1, -1)
+
+print(clf.predict(to_predict))
